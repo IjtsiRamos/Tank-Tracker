2580b79c122551f6eeb35db49cc9fa21b42a9226
IjtsiRamos <ijtsiramos96@gmail.com>
2023-06-20 21:07:43 -0600

    Bugs were fixed in the email alert feature

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post} = require('../../models');
+const { Post, User} = require('../../models');
 const nodemailer = require('nodemailer');
 
 
@@ -28,46 +28,62 @@ router.post('/add', async (req, res) => {
       user_id: req.body.user_id,
       imgWater: req.body.waterAvailable !== null ? req.body.waterAvailable : 1,
     });
-     res.status(200).json(newPost);
-    req.session.save(() => {
-      req.session.waterAvailable = newPost.waterAvailable;
-  });
+
+    
+    if (newPost.waterAvailable === 0) {
+
+      const dbUserData = await User.findOne({
+        where: {
+            id: newPost.user_id,
+        },
+    });
+
+
+      if (!dbUserData.email) {
+        return res.status(400).send(dbUserData);
+      }
+      
+      const emailRecipient= dbUserData.email;
+
+    // Check if the email recipient is empty
+      if (emailRecipient.trim() === '') {
+        return res.status(400).send('Email is empty');
+      }
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: process.env.E_USER,
+          pass: process.env.E_PASSWORD,
+        },
+      });
+    
+      // Configure the email message
+      const mailOptions = {
+        from: process.env.E_USER,
+        to: emailRecipient,
+        subject: 'Tank Tracker Data',
+        text: 'Dear Tank Tracker User, \n There is no water available to fill in your tanks, please contact your water provider.',
+      };
+    
+      // Send the email
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          res.status(500).send('Error sending email');
+        } else {
+          console.log('Email sent: ' + info.response);
+          res.status(200).send('Email sent successfully');
+          res.redirect('/');
+        }
+      });
+    }
+
+    res.status(200).json(newPost);
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.post('/alerta', (req, res) => {
-
-  // Create a Nodemailer transporter
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: 'process.env.E_USER',
-      pass: 'process.env.E_PASSWORD',
-    },
-  });
-
-  // Configure the email message
-  const mailOptions = {
-    from: 'your-email@gmail.com',
-    to: req.session.email,
-    subject: 'Tank Tracker Data',
-    text: 'Dear Tank Tracker User, \n There is no water available to fill in your tanks, please contact your water provider.',
-  };
-
-  // Send the email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent: ' + info.response);
-      res.status(200).send('Email sent successfully');
-      res.redirect('/');
-    }
-  });
-});
 
 
 
